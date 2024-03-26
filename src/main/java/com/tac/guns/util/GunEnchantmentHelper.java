6ec005fea71249511c6b90d7865d6d4e76e159f5
Shunta-Iida <meshida.0217@gmail.com>
2024-03-26 12:43:46 +0900

    エンチャント効果の変更

--- src/main/java/com/tac/guns/util/GunEnchantmentHelper.java
+++ src/main/java/com/tac/guns/util/GunEnchantmentHelper.java
@@ -1,54 +1,58 @@
 package com.tac.guns.util;
 
+import java.util.HashMap;
+
 import com.tac.guns.client.screen.UpgradeBenchScreen;
-import com.tac.guns.common.Gun;
 import com.tac.guns.init.ModEnchantments;
 import com.tac.guns.item.GunItem;
+
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
-
-import java.util.HashMap;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 /**
  * Author: Forked from MrCrayfish, continued by Timeless devs
  */
 public class GunEnchantmentHelper {
     // UNSAFE, ME NO LIKEY - CLUMSYALIEN
-    public static HashMap<String, UpgradeBenchScreen.RequirementItem> upgradeableEnchs = new HashMap<String, UpgradeBenchScreen.RequirementItem>() {
-        {
-            put("Over Pressured",
-                    new UpgradeBenchScreen.RequirementItem(new int[] { 2, 3, 3, 5 }, new int[] { 5, 7, 10, 15 },
-                            ModEnchantments.ACCELERATOR.get()));
-            /*
-             * put("Over Capacity",
-             * new UpgradeBenchScreen.RequirementItem(new int[]{1,2,3}, new int[]{1,3,6},
-             * ModEnchantments.OVER_CAPACITY.get()));
-             */
-            put("Advanced Rifling",
-                    new UpgradeBenchScreen.RequirementItem(new int[] { 1, 2, 3 }, new int[] { 4, 6, 8 },
-                            ModEnchantments.RIFLING.get()));
-            put("Buffered",
-                    new UpgradeBenchScreen.RequirementItem(new int[] { 2, 4 }, new int[] { 5, 11 },
-                            ModEnchantments.BUFFERED.get()));
-            put("Puncturing",
-                    new UpgradeBenchScreen.RequirementItem(new int[] { 1, 2, 3, 4, 5, 6 },
-                            new int[] { 4, 6, 8, 11, 14, 17 },
-                            ModEnchantments.PUNCTURING.get()));
-            put("Lightweight",
-                    new UpgradeBenchScreen.RequirementItem(new int[] { 1, 2, 3 }, new int[] { 4, 6, 8 },
-                            ModEnchantments.LIGHTWEIGHT.get()));
-        }
-    };
+    public static HashMap<String, UpgradeBenchScreen.RequirementItem> upgradeableEnchs =
+            new HashMap<String, UpgradeBenchScreen.RequirementItem>() {
+                {
+                    this.put("Over Pressured",
+                            new UpgradeBenchScreen.RequirementItem(new int[] {2, 3, 3, 5},
+                                    new int[] {5, 7, 10, 15}, ModEnchantments.ACCELERATOR.get()));
+                    /*
+                     * put("Over Capacity",
+                     * new UpgradeBenchScreen.RequirementItem(new int[]{1,2,3}, new int[]{1,3,6},
+                     * ModEnchantments.OVER_CAPACITY.get()));
+                     */
+                    this.put("Advanced Rifling",
+                            new UpgradeBenchScreen.RequirementItem(new int[] {1, 2, 3},
+                                    new int[] {4, 6, 8}, ModEnchantments.RIFLING.get()));
+                    this.put("Buffered", new UpgradeBenchScreen.RequirementItem(new int[] {2, 4},
+                            new int[] {5, 11}, ModEnchantments.BUFFERED.get()));
+                    this.put("Puncturing",
+                            new UpgradeBenchScreen.RequirementItem(new int[] {1, 2, 3, 4, 5, 6},
+                                    new int[] {4, 6, 8, 11, 14, 17},
+                                    ModEnchantments.PUNCTURING.get()));
+                    this.put("Lightweight",
+                            new UpgradeBenchScreen.RequirementItem(new int[] {1, 2, 3},
+                                    new int[] {4, 6, 8}, ModEnchantments.LIGHTWEIGHT.get()));
+                }
+            };
 
-    public static int getReloadInterval(ItemStack weapon) {
-        int interval = ((GunItem) weapon.getItem()).getGun().getReloads().getinterReloadPauseTicks();
-        return Math.max(1, Mth.floor((float) interval / getReloadSpeed(weapon)));
+    public static int getReloadInterval(final ItemStack weapon) {
+        final int interval =
+                ((GunItem) weapon.getItem()).getGun().getReloads().getinterReloadPauseTicks();
+        return Math.max(1,
+                Mth.floor((float) interval / GunEnchantmentHelper.getReloadSpeed(weapon)));
     }
 
-    public static float getReloadSpeed(ItemStack weapon) {
+    public static float getReloadSpeed(final ItemStack weapon) {
         float speed = 1.0F;
-        int level = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.QUICK_CHARGE, weapon);
+        final int level =
+                EnchantmentHelper.getItemEnchantmentLevel(Enchantments.QUICK_CHARGE, weapon);
         if (level > 0) {
             speed = (float) Math.pow(1.2, level);
         }
@@ -56,56 +60,63 @@ public class GunEnchantmentHelper {
         return speed;
     }
 
-    public static double getAimDownSightSpeed(ItemStack weapon) {
-        int level = EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.LIGHTWEIGHT.get(), weapon);
+    public static double getAimDownSightSpeed(final ItemStack weapon) {
+        final int level = EnchantmentHelper
+                .getItemEnchantmentLevel(ModEnchantments.LIGHTWEIGHT.get(), weapon);
         if (level > 0) {
-            return 1.0 + (0.075 * level);
+            return Math.pow(1.4, level);
         }
         return 1;
     }
 
-    public static float getSpreadModifier(ItemStack weapon) {
-        int level = EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.RIFLING.get(), weapon);
+    public static float getSpreadModifier(final ItemStack weapon) {
+        final int level =
+                EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.RIFLING.get(), weapon);
         if (level > 0) {
-            return 1.0f - (0.0333f * level);
+            return (float) Math.pow(0.7, level);
         }
         return 1f;
     }
 
-    public static float getWeightModifier(ItemStack weapon) {
-        int level = EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.LIGHTWEIGHT.get(), weapon);
+    public static float getWeightModifier(final ItemStack weapon) {
+        final int level = EnchantmentHelper
+                .getItemEnchantmentLevel(ModEnchantments.LIGHTWEIGHT.get(), weapon);
         if (level > 0) {
-            return 0.4f * level;
+            return (float) (Math.pow(1.4, level) - 1f);
         }
         return 0f;
     }
 
-    public static double getProjectileSpeedModifier(ItemStack weapon) {
-        int level = EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.ACCELERATOR.get(), weapon);
+    public static double getProjectileSpeedModifier(final ItemStack weapon) {
+        final int level = EnchantmentHelper
+                .getItemEnchantmentLevel(ModEnchantments.ACCELERATOR.get(), weapon);
         if (level > 0) {
             return 1.0 + 0.0333 * level;
         }
         return 1.0;
     }
 
-    public static float getAcceleratorDamage(ItemStack weapon, float damage) {
-        int level = EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.ACCELERATOR.get(), weapon);
+    public static float getAcceleratorDamage(final ItemStack weapon, final float damage) {
+        final int level = EnchantmentHelper
+                .getItemEnchantmentLevel(ModEnchantments.ACCELERATOR.get(), weapon);
         if (level > 0) {
             return damage + damage * (0.05F * level);
         }
         return damage;
     }
 
-    public static float getBufferedRecoil(ItemStack weapon) {
-        int level = EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.BUFFERED.get(), weapon);
+    public static float getBufferedRecoil(final ItemStack weapon) {
+        final int level =
+                EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.BUFFERED.get(), weapon);
         if (level > 0) {
             return (1 - (0.15F * level));
         }
         return 1;
     }
 
-    public static float getPuncturingChance(ItemStack weapon) {
-        int level = EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.PUNCTURING.get(), weapon);
+    public static float getPuncturingChance(final ItemStack weapon) {
+        final int level =
+                EnchantmentHelper.getItemEnchantmentLevel(ModEnchantments.PUNCTURING.get(), weapon);
         return level * 0.05F;
     }
 }
